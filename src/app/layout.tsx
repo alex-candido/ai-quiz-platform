aacd324c8fb8068a636ff77edaeb2a44383fc02a
Alex Cândido <alex.candido.tec@gmail.com>
2024-01-26 12:37:36 -0300

    adicionado estrutura de formatTimeDelta em  src/lib/utils.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,16 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 import '@/@server/shared/container/index';
+import { cn } from '@/lib/utils';
 import ReduxProvider from '@/providers/ReduxProvider';
+import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Quizplatform",
+  description: "Generate by create next app",
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={cn(inter.className, "antialiased min-h-screen pt-16")}>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
     </html>
