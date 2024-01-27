c0870e0f24afbf47de5c65375eeb5ddfa2e9ea2f
Alex Cândido <alex.candido.tec@gmail.com>
2024-01-27 13:27:41 -0300

    adicionado estrutura de HotTopicsCard em src/components/dashboard/HotTopicsCard.tsx

--- src/components/dashboard/HotTopicsCard.tsx
+++ src/components/dashboard/HotTopicsCard.tsx
@@ -1,8 +1,25 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const HotTopicsCard = () => {
   return (
-    <div>HotTopicsCard</div>
-  )
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle className="text-2xl font-bold">Hot Topics</CardTitle>
+        <CardDescription>
+          Click on a topic to start a quiz on it.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="pl-2">
+
+      </CardContent>
+    </Card>
+  );
 }
 
 export default HotTopicsCard
