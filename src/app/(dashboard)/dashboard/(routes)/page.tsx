ba8e550bdbda8556d3bf21b022345a4eaa56a105
Alex Cândido <alex.candido.tec@gmail.com>
2024-01-27 13:19:27 -0300

    adicionado estrutura de DetailsDialog em  src/components/DetailsDialog.tsx

--- src/app/(dashboard)/dashboard/(routes)/page.tsx
+++ src/app/(dashboard)/dashboard/(routes)/page.tsx
@@ -1,17 +1,38 @@
+import { getAuthSession } from '@/lib/session';
+import { redirect } from 'next/navigation';
+
+import DetailsDialog from '@/components/DetailsDialog';
+import HistoryCard from '@/components/dashboard/HistoryCard';
+import HotTopicsCard from '@/components/dashboard/HotTopicsCard';
+import QuizMeCard from '@/components/dashboard/QuizMeCard';
+import RecentActivityCard from '@/components/dashboard/RecentActivityCard';
+
 export const metadata = {
   title: 'Dashboard | Exams',
   description: 'Exams yourself on anything!',
 };
 
 export default async function DashboardPage() {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    redirect('/');
+  }
+
   return (
     <main className="p-8 mx-auto max-w-7xl">
       <div className="flex items-center">
         <h2 className="mr-2 text-3xl font-bold tracking-tight">Dashboard</h2>
+        <DetailsDialog />
       </div>
 
-      <div className="grid gap-4 mt-4 md:grid-cols-2"></div>
-      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7"></div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizMeCard />
+        <HistoryCard />
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+        <HotTopicsCard />
+        <RecentActivityCard />
+      </div>
     </main>
   );
 }
