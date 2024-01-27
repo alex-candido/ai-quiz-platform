fc16d43d3017fcc139f2bc6df56a3b1c4d68c58f
Alex Cândido <alex.candido.tec@gmail.com>
2024-01-27 01:37:19 -0300

    adicionado estrutura da Homepage em src/app/(root)/(routes)/page.tsx

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,27 +1,36 @@
-'use client';
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-import { useEffect } from 'react';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-import { createQuestionsAsync } from '@/redux/actions/questions/create-questions';
-import { RootState, useAppDispatch, useAppSelector } from '@/redux/store';
+import SignInButton from "@/components/SignInButton";
 
-export default function SetupPage() {
-  const dispatch = useAppDispatch();
-  const question = useAppSelector(
-    (state: RootState) => state.questions.question,
-  );
-
-  useEffect(() => {
-    dispatch(createQuestionsAsync());
-  }, [dispatch]);
+export default async function Homepage() {
+  const session = await getServerSession();
+  if (session?.user) {
+    redirect("/dashboard");
+  }
 
   return (
-    <div className="section">
-      <div className="container">
-        <div>SetupPage</div>
-        <br />
-        <div>{question.question}</div>
-      </div>
+    <div className="absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <Card className="w-[300px]">
+        <CardHeader>
+          <CardTitle>Welcome to Exames 🔥!</CardTitle>
+          <CardDescription>
+            Exame is a platform for creating exames using AI!. Get started by
+            loggin in below!
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <SignInButton text="Sign In with Google" />
+        </CardContent>
+      </Card>
     </div>
   );
 }
