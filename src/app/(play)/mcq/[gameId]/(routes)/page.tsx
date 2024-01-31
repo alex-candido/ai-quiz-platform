dc9f5e40e3603163b9be8cc23d3ae7c90b299806
Alex Cândido <alex.candido.tec@gmail.com>
2024-01-30 23:37:55 -0300

    adicionado estrutura de mcq em src/components/MCQ.tsx

--- src/app/(play)/mcq/[gameId]/(routes)/page.tsx
+++ src/app/(play)/mcq/[gameId]/(routes)/page.tsx
@@ -1,9 +1,41 @@
-export default function McqPage() {
+import { getServerSession } from "next-auth";
+import { redirect } from 'next/navigation';
+
+import MCQ from "@/components/MCQ";
+import prisma from "@/config/data-source";
+
+interface McqPageProps {
+  params: {
+    gameId: string;
+  };
+}
+
+export default async function McqPage({ params: { gameId } }:McqPageProps ) {
+  const session = await getServerSession();
+  if (!session?.user) {
+    redirect("/");
+  }
+
+  const game = await prisma.game.findUnique({
+    where: {
+      id: gameId,
+    },
+    include: {
+      questions: {
+        select: {
+          id: true,
+          question: true,
+          options: true,
+        },
+      },
+    },
+  });
+
+  if (!game || game.gameType === "open_ended") {
+    return redirect("/quiz");
+  }
+
   return (
-    <div className="section">
-      <div className="container">
-        <div>McqPage</div>
-      </div>
-    </div>
+    <MCQ game={game} />
   );
 }
