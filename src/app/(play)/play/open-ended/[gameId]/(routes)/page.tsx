ebc2782c9da8f2cbfa7ea195b9d3ed718f6c493a
Alex Cândido <alex.candido.tec@gmail.com>
2024-02-01 01:01:03 -0300

    adicionado estrutura de open-ended page em  src/app/(play)/play/open-ended/[gameId]/(routes)/page.tsx

--- src/app/(play)/play/open-ended/[gameId]/(routes)/page.tsx
+++ src/app/(play)/play/open-ended/[gameId]/(routes)/page.tsx
@@ -1,9 +1,40 @@
-export default function OpenEndedPage() {
+import OpenEnded from "@/components/OpenEnded";
+import prisma from "@/config/data-source";
+import { getAuthSession } from "@/config/next-auth";
+import { redirect } from "next/navigation";
+
+interface OpenEndedPageProps {
+  params: {
+    gameId: string;
+  };
+}
+
+export default async function OpenEndedPage({ params: { gameId } }: OpenEndedPageProps) {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  const game = await prisma.game.findUnique({
+    where: {
+      id: gameId,
+    },
+    include: {
+      questions: {
+        select: {
+          id: true,
+          question: true,
+          answer: true,
+        },
+      },
+    },
+  });
+
+  if (!game || game.gameType === "mcq") {
+    return redirect("/quiz");
+  }
+
   return (
-    <div className="section">
-      <div className="container">
-        <div>OpenEndedPage</div>
-      </div>
-    </div>
+    <OpenEnded game={game} />
   );
 }
